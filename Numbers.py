8de103a1b9fc69b03d48c77d9f446d299ba5704f
Mallikarjun Reddy <reddymallireddy9701@gmail.com.com>
2021-05-01 15:52:07 +0530

    Numbers in python

---
+++ Numbers.py
@@ -0,0 +1,36 @@
+##Python Numbers
+
+There are three numeric types in Python:
+int
+float
+complex
+
+Example:
+x = 1
+y = 2.8
+z = 1j
+print(type(x))
+print(type(y))
+print(type(z))
+
+*** Output ***
+<class 'int'>
+<class 'float'>
+<class 'complex'>
+
+----------------------------------------------------------------------------------------------
+
+## Random Number
+
+Python has a built-in module called random that can 
+be used to make random numbers:
+
+Example:
+
+# Import the random module, and display a random number 
+   between 1 and 9:
+
+import random
+print(random.randrange(1, 10))
+
+-------------------------------------------------------------------------------------------------
