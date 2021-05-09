6fb4fdc87036e82317b9b2fd4f988b0ffb4e5674
Mallikarjun Reddy <reddymallireddy9701@gmail.com.com>
2021-05-09 18:02:30 +0530

    Pime Number program with interval

--- Programs/Prime.py
+++ Programs/Prime.py
@@ -32,3 +32,27 @@ else:
    Prime
 -> Enter a number: 9
    Not a prime
+
+   ----------------------------------------------------------------------------------------------------------
+
+PRINTING PRIME NUMBERS IN GIVEN INTERVAL
+
+lower = int(input("Enter the lower interval:"))
+upper = int(input("Enter the upper interval:"))
+for num in range(lower, upper + 1):
+    if num > 1:
+        for i in range(2, num):
+            if num % i == 0:
+                break
+        else:
+            print(num)
+
+->  Enter the lower interval:5
+    Enter the upper interval:15
+    5
+    7
+    11
+    13
+
+
+
