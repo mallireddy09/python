fe4c83bc9b92c72a2e4688f5092c29c0d8c99217
Mallikarjun Reddy <reddymallireddy9701@gmail.com.com>
2021-05-12 22:49:15 +0530

    Basic Interview Questions in python

---
+++ Programs/interview.py
@@ -0,0 +1,98 @@
+
+1 What is Python?
+
+2 What are the benefits of Python?
+
+3 What are the key features of Python?
+
+4 What type of language is Python? Programming or Scripting?
+
+5 What are the applications of Python?
+
+6 What is the difference between list and tuple in Python?
+
+7 What are the global and local variables in Python?
+
+8 Define PYTHON PATH?
+
+9 What are the two major loop statements?
+
+10 What do you understand by the term PEP 8?
+
+11 How memory management is done in Python? 
+
+12 What is the principal difference between Java and Python?
+
+13 Define modules in Python?
+
+14 What are the built-in types available in Python?
+
+15 What are Python Decorators?
+
+16 How do we find bugs and statistical problems in Python?
+
+17 What is the difference between .py and .pyc files? 
+
+18 How do you invoke the Python interpreter for interactive use?
+
+19 Define String in Python?
+
+20 What do you understand by the term namespace in Python?
+
+21 How do you create a Python function?
+
+22 Define iterators in Python?
+
+23 How does a function return values?
+
+24 Define slicing in Python?
+
+25 How can Python be an interpreted language?
+ 
+26 function without return is valid?
+
+27 Define package in Python?
+
+28 How can we make a Python script executable on Unix?
+
+29 Which command is used to delete files in Python?
+
+30 Define pickling and unpickling in Python?
+
+31 Explain the difference between local and global namespaces?
+
+32 What is a boolean in Python?
+
+33 What is Python String format and Python String replace?
+
+34 Name some of the built-in modules in Python?
+
+35 What are the functions in Python?
+
+36 What are Dict and List comprehensions in Python?
+
+37 Define the term lambda?
+
+38 When would you use triple quotes as a delimiter? 39 Define self in Python?
+
+40 What is init?
+
+41. How do you debug a Python program? 
+
+42. What is <Yield> Keyword in Python?
+
+43. How to convert a list into a string?
+
+44. How to convert a list into a tuple?
+
+45. How to convert a list into a set?
+
+46. How to count the occurrences of a particular element in the list?
+
+47. What is NumPy array?
+
+48. How can you create Empty NumPy Array In Python?
+
+49. What is a negative index in Python?
+
+50. How do you Concatenate Strings in Python?
