e7eab7caf0d4e8049ca7689591d3c01ab3a98258
Mallikarjun Reddy <reddymallireddy9701@gmail.com.com>
2021-05-14 00:18:19 +0530

    Bin 2 dec in python

--- Programs/BinaryToDecimal.py
+++ Programs/BinaryToDecimal.py
@@ -13,3 +13,41 @@ print("Decimal Number is: ",sum)
 ->  Enter a Binary Number: 105
     Decimal Number is:  9
 
+-------------------------------------------------------------------------------------
+
+def DecimalToBinary(num):
+
+    if num >= 1:
+        DecimalToBinary(num // 2)
+    print(num % 2, end = '')
+
+dec_val = int(input("Enter a Decimal Number: "))
+DecimalToBinary (dec_val)
+
+->  Enter a Decimal Number: 7
+    0111
+
+--------------------------------------------------------------------
+
+def binaryToDecimal(binary):
+    binary1 = binary 
+    decimal, i, n = 0, 0, 0
+    while(binary != 0):
+        dec = binary % 10
+        decimal = decimal + dec* pow(2, i)
+        binary = binary//10
+        i += 1
+    print(decimal)
+
+# Calling function
+binary = int(input("Enter a Binary Number: "))
+binaryToDecimal(binary)
+
+->  Enter a Binary Number: 105
+    9
+----------------------------------------------------------------
+
+# Simple way to convert / Trick
+
+print(int("01001",2))
+-> 9
