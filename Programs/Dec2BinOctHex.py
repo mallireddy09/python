44a7b3e7174b432b34e1c1b957cd7ef0a168c81a
Mallikarjun Reddy <reddymallireddy9701@gmail.com.com>
2021-05-13 23:43:19 +0530

    decimal to binary, octal and hexadecimal in python

--- Programs/Dec2BinOctHex.py
+++ Programs/Dec2BinOctHex.py
@@ -1,3 +1,4 @@
+"""
 decimal = int(input("Enter a Decimal Number: "))
 convert = int(input("Convert into: [1] Binary, [2] Octal [3] HexaDecimal: \n"))
 
@@ -22,3 +23,57 @@ else:
     Convert into: [1] Binary, [2] Octal [3] HexaDecimal: 
     3
     Converted to HexaDecimal:  0x3
+
+-----------------------------------------------------------------------------------------------------------------
+
+decimal = int(input("Enter a Decimal Number: "))
+
+print(bin(decimal), "in Binary.")
+print(oct(decimal), "in Octal.")
+print(hex(decimal), "in HexaDecimal.")
+
+->  Enter a Decimal Number: 3
+    0b11 in Binary.
+    0o3 in Octal.
+    0x3 in HexaDecimal.
+-----------------------------------------------------------------------------------------------------------------
+
+"""
+# Python program to convert decimal to binary, octal and hexadecimal 
+
+  
+# Function to convert decimal to binary 
+
+def decimal_to_binary(dec): 
+
+    decimal = int(dec) 
+    # Prints equivalent decimal 
+    print(decimal, " in Binary : ", bin(decimal)) 
+
+  
+# Function to convert decimal to octal 
+
+def decimal_to_octal(dec): 
+    decimal = int(dec) 
+    # Prints equivalent decimal 
+    print(decimal, " in Octal : ", oct(decimal)) 
+
+  
+# Function to convert decimal to hexadecimal 
+
+def decimal_to_hexadecimal(dec): 
+    decimal = int(dec) 
+    # Prints equivalent decimal 
+    print(decimal, " in Hexadecimal : ", hex(decimal)) 
+
+dec = int(input("Enter a Decimal Number: "))
+decimal_to_binary(dec) 
+decimal_to_octal(dec) 
+decimal_to_hexadecimal(dec)
+
+->  Enter a Decimal Number: 3
+    3  in Binary :  0b11
+    3  in Octal :  0o3
+    3  in Hexadecimal :  0x3
+
+--------------------------------------------------------------------------------------------------------------------------------------------
