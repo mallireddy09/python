8061fc2c1b6d5756696815d3259b64e595118da3
Mallikarjun Reddy <reddymallireddy9701@gmail.com.com>
2021-05-22 16:12:45 +0530

    Multiplicaation table using python

---
+++ Programs/Multiplication.py
@@ -0,0 +1,19 @@
+
+# To take input from the user
+num = int(input("Number :2"))
+
+# Iterate 10 time from i = 1 to 10
+for i in range(1, 11):
+    print(num, 'x', i, '=', num*i)
+
+->  Number :11
+    11 x 1 = 11
+    11 x 2 = 22
+    11 x 3 = 33
+    11 x 4 = 44
+    11 x 5 = 55
+    11 x 6 = 66
+    11 x 7 = 77
+    11 x 8 = 88
+    11 x 9 = 99
+    11 x 10 = 110
