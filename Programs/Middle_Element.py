9a4c8d224e3a89230e0b2902f6bf52a8d01ec9b5
Mallikarjun Reddy <reddymallireddy9701@gmail.com.com>
2021-06-01 16:22:23 +0530

    Middle element in the string

---
+++ Programs/Middle_Element.py
@@ -0,0 +1,13 @@
+def middle(n):
+    if len(n)%2==0:
+        print(n[len(n)//2-1]+n[len(n)//2])
+    else:
+        print(n[len(n)//2])
+n=input()
+middle(n)
+
+========= Output ===========
+ 1. world
+    r
+2.  python 
+    th
